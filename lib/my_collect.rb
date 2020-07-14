b40b4ace97fc04adfd74abb36526203e49316335
Katie Le <lethikatie@gmail.com>
2020-07-14 05:09:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(languages)
+  students = []
+  i = 0 
+  while i < languages.length
+  students << yield(languages[i])
+  my_collect(students) do |student|
+  i += 1
+  end 
+  students
+  end
+end 
